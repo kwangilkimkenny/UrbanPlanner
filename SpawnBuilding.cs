4d9bb26f37660070d6bc86a7b5153c4fadfc4176
kwangilKyle <38448277+kwangilkimkenny@users.noreply.github.com>
2021-09-09 19:57:35 +0900

    Add files via upload

--- SpawnBuilding.cs
+++ SpawnBuilding.cs
@@ -58,42 +58,37 @@ public class SpawnBuilding : MonoBehaviour
     // 블럭에 빌딩생성을 위한 면적범위의 원 반지름 값 리스트로 81개가 되어야 spawnBD_Raius_All
     public List<float> spawnBD_Raius_All = new List<float>();
 
-    // private bool state = true;
+    private bool state = true;
 
 
     public void StartSpawn()
     {
 
 
-        SpawnPos_FindIntersection();
+        //SpawnPos_FindIntersection();
 
-        //Debug.Log("Start!");
-        // 빌딩이 위치할 지점의 중심지점값들을 계산 > spawnLocations  리스트로 모두 추출 81개임
+        //Spawn();
+        ResetAllBuilding();
 
-        Spawn();
+        if (state == true) // 즉, 스폰되어 있지 않았다면 스폰한다.
+        {
+            SpawnPos_FindIntersection();
 
-        //if (state == true) // 즉, 스폰되어 있지 않았다면 스폰한다.
-        //{
-        //    SpawnPos_FindIntersection();
+            Spawn();
 
-        //    //Debug.Log("Start!");
-        //    // 빌딩이 위치할 지점의 중심지점값들을 계산 > spawnLocations  리스트로 모두 추출 81개임
+            state = false;
+        }
+        else // true 라면, 즉, 스폰되었다면 
+        {
 
-        //    Spawn();
+            ResetAllBuilding();
 
-        //    // 스폰이 되면 true로 하고, else에서 ReSpawn을 실행하면 생성한 건물을 껐다 켰다 할 수 있음(재활용가능)
-        //    state = false;
-        //}
-        //else // true 라면, 즉, 스폰되었다면 
-        //{
-        //    Debug.Log("state :" + state);
-        //    //ReSpawn();
+            // 아예 새로 만들어 버린다.
+            SpawnPos_FindIntersection();
+            Spawn();
 
-
-        //    // 아예 새로 만들어 버린다.
-        //    SpawnPos_FindIntersection();
-        //    Spawn();
-        //}
+            state = true;
+        }
 
     }
 
@@ -328,128 +323,128 @@ public class SpawnBuilding : MonoBehaviour
 
 
 
-        // Find the intersection of two lines - 빌딩생성을 위한 블럭당 지형의 중심위치 추출
-        static Vector3 FindIntersection(Vector3 s1, Vector3 e1, Vector3 s2, Vector3 e2)
+    // Find the intersection of two lines - 빌딩생성을 위한 블럭당 지형의 중심위치 추출
+    static Vector3 FindIntersection(Vector3 s1, Vector3 e1, Vector3 s2, Vector3 e2)
+    {
+        float a1 = e2.z - s1.z;
+        float b1 = s1.x - e2.x;
+        float c1 = a1 * s1.x + b1 * s1.z;
+
+        float a2 = e1.z - s2.z;
+        float b2 = s2.x - e1.x;
+        float c2 = a2 * s2.x + b2 * s2.z;
+
+        float delta = a1 * b2 - a2 * b1;
+        //If lines are parallel, the result will be (NaN, NaN).
+        return delta == 0 ? new Vector3(float.NaN, float.NaN, float.NaN)
+            : new Vector3((b2 * c1 - b1 * c2) / delta, 0, (a1 * c2 - a2 * c1) / delta);
+    }
+
+
+
+
+    // 생성된 gioPos의 위치값을 추출하여 블럭으로 재구성(1block = 4giPos)으로 총 100개의 포인트가 81개의 블럭으로 구성되고, 각 블럭의 중심위치를 추출하여 (중심점에서 빌딩을 생성)
+    public void SpawnPos_FindIntersection()
+    {
+
+        // GioPos 태그를 가진 GameObject를 모두 찾아서 새로운 리스트에 하나씩 담는다. 이것은 위치값을 추적하기 위함이다.
+        foreach (GameObject spawnp in GameObject.FindGameObjectsWithTag("GioPos"))
         {
-            float a1 = e2.z - s1.z;
-            float b1 = s1.x - e2.x;
-            float c1 = a1 * s1.x + b1 * s1.z;
-
-            float a2 = e1.z - s2.z;
-            float b2 = s2.x - e1.x;
-            float c2 = a2 * s2.x + b2 * s2.z;
-
-            float delta = a1 * b2 - a2 * b1;
-            //If lines are parallel, the result will be (NaN, NaN).
-            return delta == 0 ? new Vector3(float.NaN, float.NaN, float.NaN)
-                : new Vector3((b2 * c1 - b1 * c2) / delta, 0, (a1 * c2 - a2 * c1) / delta);
+            spawnPrefabs.Add(spawnp);
+            //Debug.Log("check 1 !");
         }
 
-
-
-
-        // 생성된 gioPos의 위치값을 추출하여 블럭으로 재구성(1block = 4giPos)으로 총 100개의 포인트가 81개의 블럭으로 구성되고, 각 블럭의 중심위치를 추출하여 (중심점에서 빌딩을 생성)
-        public void SpawnPos_FindIntersection()
+        // 4 지점의 조합 리스트값이 필요함. ---> Blocks로 계산해야 ㅎ
+        for (int i = 0; i < spawnPrefabs.Count - 11; i++)
         {
 
-            // GioPos 태그를 가진 GameObject를 모두 찾아서 새로운 리스트에 하나씩 담는다. 이것은 위치값을 추적하기 위함이다.
-            foreach (GameObject spawnp in GameObject.FindGameObjectsWithTag("GioPos"))
+            if (i == 9 || i == 19 || i == 29 || i == 39 || i == 49 || i == 59 || i == 69 || i == 79 || i == 89 || i == 99 || i == 109)
             {
-                spawnPrefabs.Add(spawnp);
-                //Debug.Log("check 1 !");
+                continue;
             }
 
-            // 4 지점의 조합 리스트값이 필요함. ---> Blocks로 계산해야 ㅎ
-            for (int i = 0; i < spawnPrefabs.Count - 11; i++)
-            {
-
-                if (i == 9 || i == 19 || i == 29 || i == 39 || i == 49 || i == 59 || i == 69 || i == 79 || i == 89 || i == 99 || i == 109)
-                {
-                    continue;
-                }
-
-                interSecPos.Add(spawnPrefabs[i].transform.position);
-                interSecPos.Add(spawnPrefabs[i + 1].transform.position);
-                interSecPos.Add(spawnPrefabs[i + 10].transform.position);
-                interSecPos.Add(spawnPrefabs[i + 11].transform.position);
-
-            }
-
-            Debug.Log("interSecPos.Count" + interSecPos.Count); // 324개로 4로 나누면 81개, 9X9의 블럭으로 구성되었기 때문
-                                          // 4개의 지점으로 블럭조합 리스트가 완성되면 (,,,)의 리스트로 그룹데이터 생성됨, 이것을 가지고 중간지점  == > interSecPos
-
-            // 지도상의 블럭 즉, 4지점을 추출하기 위한 gameobject르 순서대로 묶은 리스트 값 선언 
-            int k = 0;
-            for (int i = 0; i <= interSecPos.Count / 4 - 1; i++)
-            {
-                //Debug.Log("interSecPos[k]" + interSecPos[k]); // 위치값이 잘 나옴
-                //Debug.Log("interSecPos[k]" + interSecPos[k+1]);
-                //Debug.Log("interSecPos[k]" + interSecPos[k+2]);
-                //Debug.Log("interSecPos[k]" + interSecPos[k+3]);
-                Vector3 spawnLocation = FindIntersection(interSecPos[k], interSecPos[k + 1], interSecPos[k + 2], interSecPos[k + 3]);
-
-                //Debug.Log("spawnLocation" + spawnLocation);
-                // 생성위치추출로 81개의 블럭 중심위치를 리스트로 저장
-                spawnLocations.Add(spawnLocation);
-
-                // 생성위치 + 블럭의 4 지점위치 데이터를 바탕으로 블럭의 면적을 계산하고, 면적안에 빌딩을 스폰(면적안에서만 빌딩을 물리적을 위치시켜야 함), 면적보다크면 빌딩스폰 정지
-                // 벡터 연산이기 때문에 삼각형으로 나누어서 면적을 계산
-                float area1 = Area_tri(interSecPos[k], interSecPos[k + 1], interSecPos[k + 2]);
-                float area2 = Area_tri(interSecPos[k + 1], interSecPos[k + 3], interSecPos[k + 2]);
-                // 블록의 면적 : 기본크기는 1임, gioPos 단위가 1이니까. 
-                float areaOfBlock = area1 + area2;
-                //Debug.Log("블럭당 면적 : " + areaOfBlock);
-
-                //블럭당 면적으로 리스트에 저장할 것
-                allAreaValueList.Add(areaOfBlock);
-
-                //빌딩의 스폰 범위를 계산하기 위한 중심 vs 4개 지점의 최대거리값 계산하기
-                List<float> LonggistDst = new List<float>();
-
-                float D1 = Vector3.Distance(spawnLocation, interSecPos[k]);
-                LonggistDst.Add(D1);
-                //Debug.Log("D1 :" + D1);
-                float D2 = Vector3.Distance(spawnLocation, interSecPos[k + 1]);
-                LonggistDst.Add(D2);
-                float D3 = Vector3.Distance(spawnLocation, interSecPos[k + 2]);
-                LonggistDst.Add(D3);
-
-                float D4 = Vector3.Distance(spawnLocation, interSecPos[k + 3]);
-                LonggistDst.Add(D4);
-
-                // 가장큰 값 취득
-                //Debug.Log("Max : " + LonggistDst.Max());
-                // 스폰생성의 반지름 개별값
-                float spawnBD_Raius = LonggistDst.Max();
-                // 리스트로 저장 spawnBD_Raius_All
-                spawnBD_Raius_All.Add(spawnBD_Raius); 
-
-        k += 4;
-            }
+            interSecPos.Add(spawnPrefabs[i].transform.position);
+            interSecPos.Add(spawnPrefabs[i + 1].transform.position);
+            interSecPos.Add(spawnPrefabs[i + 10].transform.position);
+            interSecPos.Add(spawnPrefabs[i + 11].transform.position);
 
         }
 
+        Debug.Log("interSecPos.Count" + interSecPos.Count); // 324개로 4로 나누면 81개, 9X9의 블럭으로 구성되었기 때문
+                                        // 4개의 지점으로 블럭조합 리스트가 완성되면 (,,,)의 리스트로 그룹데이터 생성됨, 이것을 가지고 중간지점  == > interSecPos
 
-        // 벡터 3 지점의 내적 계산
-        static float Area_tri(Vector3 a, Vector3 b, Vector3 c)
+        // 지도상의 블럭 즉, 4지점을 추출하기 위한 gameobject르 순서대로 묶은 리스트 값 선언 
+        int k = 0;
+        for (int i = 0; i <= interSecPos.Count / 4 - 1; i++)
         {
-            Vector3 side1;
-            side1 = b - a;
+            //Debug.Log("interSecPos[k]" + interSecPos[k]); // 위치값이 잘 나옴
+            //Debug.Log("interSecPos[k]" + interSecPos[k+1]);
+            //Debug.Log("interSecPos[k]" + interSecPos[k+2]);
+            //Debug.Log("interSecPos[k]" + interSecPos[k+3]);
+            Vector3 spawnLocation = FindIntersection(interSecPos[k], interSecPos[k + 1], interSecPos[k + 2], interSecPos[k + 3]);
 
-            Vector3 side2;
-            side2 = c - a;
+            //Debug.Log("spawnLocation" + spawnLocation);
+            // 생성위치추출로 81개의 블럭 중심위치를 리스트로 저장
+            spawnLocations.Add(spawnLocation);
 
-            Vector3 prep;
-            prep = Vector3.Cross(side1, side2);
+            // 생성위치 + 블럭의 4 지점위치 데이터를 바탕으로 블럭의 면적을 계산하고, 면적안에 빌딩을 스폰(면적안에서만 빌딩을 물리적을 위치시켜야 함), 면적보다크면 빌딩스폰 정지
+            // 벡터 연산이기 때문에 삼각형으로 나누어서 면적을 계산
+            float area1 = Area_tri(interSecPos[k], interSecPos[k + 1], interSecPos[k + 2]);
+            float area2 = Area_tri(interSecPos[k + 1], interSecPos[k + 3], interSecPos[k + 2]);
+            // 블록의 면적 : 기본크기는 1임, gioPos 단위가 1이니까. 
+            float areaOfBlock = area1 + area2;
+            //Debug.Log("블럭당 면적 : " + areaOfBlock);
 
-            float perpLength;
-            perpLength = prep.magnitude;
+            //블럭당 면적으로 리스트에 저장할 것
+            allAreaValueList.Add(areaOfBlock);
 
-            float areaResult = perpLength / 2;
+            //빌딩의 스폰 범위를 계산하기 위한 중심 vs 4개 지점의 최대거리값 계산하기
+            List<float> LonggistDst = new List<float>();
 
-            return areaResult;
+            float D1 = Vector3.Distance(spawnLocation, interSecPos[k]);
+            LonggistDst.Add(D1);
+            //Debug.Log("D1 :" + D1);
+            float D2 = Vector3.Distance(spawnLocation, interSecPos[k + 1]);
+            LonggistDst.Add(D2);
+            float D3 = Vector3.Distance(spawnLocation, interSecPos[k + 2]);
+            LonggistDst.Add(D3);
 
+            float D4 = Vector3.Distance(spawnLocation, interSecPos[k + 3]);
+            LonggistDst.Add(D4);
+
+            // 가장큰 값 취득
+            //Debug.Log("Max : " + LonggistDst.Max());
+            // 스폰생성의 반지름 개별값
+            float spawnBD_Raius = LonggistDst.Max();
+            // 리스트로 저장 spawnBD_Raius_All
+            spawnBD_Raius_All.Add(spawnBD_Raius); 
+
+    k += 4;
         }
+
+    }
+
+
+    // 벡터 3 지점의 내적 계산
+    static float Area_tri(Vector3 a, Vector3 b, Vector3 c)
+    {
+        Vector3 side1;
+        side1 = b - a;
+
+        Vector3 side2;
+        side2 = c - a;
+
+        Vector3 prep;
+        prep = Vector3.Cross(side1, side2);
+
+        float perpLength;
+        perpLength = prep.magnitude;
+
+        float areaResult = perpLength / 2;
+
+        return areaResult;
+
+    }
 
 
 
